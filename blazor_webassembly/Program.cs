0c796a9203c89f47fcbc47213adf37444f4b944c
unknown <dschenck@win.redhat.com>
2024-09-13 12:58:15 -0400

    More

--- blazor_webassembly/Program.cs
+++ blazor_webassembly/Program.cs
@@ -1,16 +1,10 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using qotd_frontend_blazor;
-using System.Net.Http.Json;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-// get settings
-
-// end get settings
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
